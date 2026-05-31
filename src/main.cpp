b5ac7e1b1550721879a729484713f9422b9ff903
ViingF <352159817@qq.com>
2026-05-31 16:17:35 +0800

    解决了windows控制台打印中文字符乱码的问题

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <clocale>
+
+#if defined(_WIN32)
+#include <windows.h>
+#endif
 
 #include "core/Game.hpp"
 #include "core/Logger.hpp"
 #include "config/Paths.hpp"
 
+static void setupConsoleEncoding()
+{
+#if defined(_WIN32)
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
+    std::setlocale(LC_ALL, ".UTF-8");
+}
+
 int main() {
+    setupConsoleEncoding();
+
     Logger::instance().init(
         FILEPATH + "/logs/game.log",
         LogLevel::Debug,
